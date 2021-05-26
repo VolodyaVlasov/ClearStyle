7fb68edcfbb08ba57da92a21c9c65dc0ccc375d4
volodyavlasov <ivlasovvladimir@gmail.com>
2021-05-27 01:49:43 +0300

    Initial commit

---
+++ src/com/company/One.java
@@ -0,0 +1,40 @@
+package com.company;
+
+public class One {
+//    count - countOfElementsInStack
+//    количество элементов в стеке
+//
+//    first - firstInList
+//    первый элемент в листе
+//
+//    last - indexOfLastElement
+//    последний элемент в хипе
+//
+//    person - personFromDataBase
+//    персона из базы данных
+//
+//    result - searchResult
+//    результат поиска
+//
+//    min - minValueInArray
+//    минимальнае значение в массиве
+//
+//    index - middleElementIndex
+//    индеекс среднего элемента
+//
+//    size - arraySize
+//    размер массива
+//
+//    books - soldBooks
+//    проданно книг
+//
+//    array - arrayForSort
+//    массив для сортировки
+//
+//    sum - sumOfPositiveNumbers
+//    сумма положительных значенией
+//
+//    price - priceOfBook
+//    стомость книги
+
+}

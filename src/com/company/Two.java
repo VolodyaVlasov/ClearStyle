5926e300a842cf12e0e5992b155db4c0f9f7f5bf
volodyavlasov <ivlasovvladimir@gmail.com>
2021-06-05 18:14:59 +0300

    two

---
+++ src/com/company/Two.java
@@ -0,0 +1,43 @@
+package com.company;
+
+
+public class Two {
+}
+//6.1
+// distanceInKm - distance расстояние может быть не только в км
+// quantity - quantityOfWheels
+// lengthAndWidth - landArea
+// button - Redbutton
+// peoplePerDay - views
+
+//6.2
+// stateMap
+// taskList
+// taskFilter
+// orderedList
+
+//6.3
+class Person {
+    String firstName;
+    String LastName;
+    int age;
+}
+
+class Car {
+    String mark;
+    String model;
+    int power;
+}
+
+class CarOwner {
+    Person owner;
+    Car car;
+}
+//6.4
+// personfdb - personFromDb
+// personSrv - personService
+// resList - resultList
+// respFrSer - responseFromServer
+// errContr - errorController
+
+

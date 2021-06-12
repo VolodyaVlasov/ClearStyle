727ee7feb76cf86c95216f59a110968849d03206
Vladimir <ivlasovvladimir@gmail.com>
2021-06-12 14:22:51 +0300

    3

---
+++ src/com/company/Three.java
@@ -0,0 +1,29 @@
+package com.company;
+
+public class Three {
+    //7.1
+    //  finished - isFinish
+    // changed - isChanged
+    // timeUpdate - isTimeUpdate
+    // first - isFirst
+    // check - needCheck
+
+    //7.2
+    // needStop - done - для выхода из внешнего цикла
+    // findMax - found найдено нужно число
+    // userFind - found - обьект найден
+
+    //7.3
+    // i - indexOfMin
+    // k - notEmptyList
+    // i - bucketIndex
+
+    // 7.4
+    // min - max
+    // start -finish
+
+    //7.5
+    // temp - temporaryIndexForSwap
+    // i - medianIndex
+    // p - personForNullCheck
+}

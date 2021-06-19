2b9e0505a5604a9d37847cec5c7d8e028d036643
Vladimir <ivlasovvladimir@gmail.com>
2021-06-19 18:45:30 +0300

    4

---
+++ src/com/company/Four.java
@@ -0,0 +1,18 @@
+package com.company;
+
+import java.awt.print.Pageable;
+
+public class Four {
+//    i1 -firstIndex
+//    user1-firstUser
+//    sum -totalPrice
+//    result -sortedArray
+//    UserClass - User
+//    longAge - personAge
+//    personList - Persons
+//    responseMap - response
+//    maxPrice - priceMax
+//    personNameString - name - находится в классе Person
+//    averageTime - timeAverage
+//    engineObject - engine
+}

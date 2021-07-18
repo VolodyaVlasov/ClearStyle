291d6427af3c759a2a0b2a394f3ab69d97e2f7a0
Vladimir <ivlasovvladimir@gmail.com>
2021-07-19 00:28:08 +0300

    7

---
+++ src/com/company/Seven.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Seven {
+    //3.1
+    //wellDtoFromWell - создает дто с обьекта
+    //eventWithWell(Well well) - создает обьект event и связывает его с обьектом well
+    //wellWithWellbores(Collections wellbores) создает обьект well и связывает его с обьектами wellBores
+
+    //3.2
+    // Все интерфейсы имеют полное название, wellService, eventService имплементации постфикс impl
+
+}

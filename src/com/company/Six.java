263ad9d7ea4bb6332e167a96e2df3e17ae14bda3
Vladimir <ivlasovvladimir@gmail.com>
2021-07-18 23:59:34 +0300

    6

--- src/com/company/Six.java
+++ src/com/company/Six.java
@@ -1,4 +1,16 @@
 package com.company;
 
 public class Six {
+    //updateWellLength - updateLength находится в классе Well
+    //updateTime - updateStartTime - более понятно какую переменную обновляет
+    //deleteWell - delete - находится в классе Well
+    //calculateEventTime - calculateTime - находится в классе  Event
+    //addEvent - addEventAndDeletePrevious
+    //calculateTime - calculateTimeAndSendNotification
+    //createWell - createWellAndPersistEvent
+    //sendNotification - send находится в классе notification
+    //addNewWellBoreToWell - addNewWellBore - находится в классе Well
+    //deleteWellBoreFromWell - deleteWellBore - находится в классе Well
+    //setNewTimeToWell - setNewTime - находится в классе Well
+    //time - calculateTime - более понятно что делает метод
 }

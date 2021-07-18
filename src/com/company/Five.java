ba0a01390200f7df1168dcfc318e39a2be8e924d
Vladimir <51863950+VolodyaVlasov@users.noreply.github.com>
2021-07-18 23:00:18 +0300

    Update Five.java

--- src/com/company/Five.java
+++ src/com/company/Five.java
@@ -2,5 +2,18 @@ package com.company;
 
 public class Five {
     //3.1
-
-}
+    WellManager - WellController
+    EventManager - EventController
+    SendNotification - NotficationSendler
+    CheckPermission - Permissionchecker
+    ExecuteRequest - ExecutorRequest
+        
+    //3.2
+    fetchWellName - getWellName // в других методах используется get для получения
+    WellService - WellServiceImpl // интерфейсы имеют обычное название, имплементации подпись impl 
+    WellRepo - WellDao // все классы которые работают с бд, имеют преписку dao
+    convertWellToDto - toDto // используется во всех сервисах, для конвертации в дто
+    wellCustomLogicBeforePersist - customLogicBeforePersist
+    wellAfterDeleteLogic - customLogicAfterDelete
+    wellAfterCreateLogic- customLogicAfterCreate
+    }

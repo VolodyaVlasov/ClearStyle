07792a4b849c59a350fe5cd77e9d0c5087b3d937
Vladimir <ivlasovvladimir@gmail.com>
2021-07-22 22:12:28 +0300

    8

---
+++ src/com/company/Eigth.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Eigth {
+    // i < array.length() - boolean indexLessArrayLength = i < array.length() - условие проверки выхода за пределы индекса вынесенно в отдельную переменную
+    // double duration - long duration - продолжительность в секундах
+    // double metrage - long metrage - метраж в метрах
+    // "/api/reporting" - public static final String PATH = "/api/reporting/" - путь у контроллера вынесен в отденую константу, что бы не дублировать
+    // "report" - public static final String defaultRemark = "report" - дефолтное значение вынесено в константу
+    // "SELECT * FROM report" = String SQL_GET_ALL_REPORT - запрос вынессен в отдельную константу
+    // int durationSumPerDay - long durationSumPerDay изменент тип переменной во избежания переполнения
+    // double deep - long deep
+    // double startPosition - longStartPosition
+    // "daily report" = public static final String DAILY_REPORT = "daily report" повторяющиеся значение вынесенно в константу
+    // "planned report" = public static final String PLANNED_REPORT = "planned report"
+    //  x < array[i] - boolean lessCurrent = x < array[i] - проверка вынесена в отдельную переменную
+
+}

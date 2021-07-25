ff314d15a5bdb788bf743ef4aff16e1aa6f317f0
Vladimir <ivlasovvladimir@gmail.com>
2021-07-25 23:48:14 +0300

    9

---
+++ src/com/company/Nine.java
@@ -0,0 +1,55 @@
+package com.company;
+
+public class Nine {
+//    Long countOfWellBores;
+//     объявлялась как поле класса, является размером коллекции содежщей количесво стволов, которое может менятся.
+//     перенес в  геттер
+//
+//    Long totalMetrage;
+//     объявлялась как поле класса, является суммой глубины всех скважен, на данной месторождении.
+//     перенес в  геттер
+//
+//    Long countOfRig;
+//     объявлялась как поле класса, количесво буровых установок на скважене.
+//     перенес в  геттер
+//
+//    Long serialNumber; - final Long serialNumber;
+//    сделал final так как поле устанавлиется один раз при создании
+//
+//    String name; - final String name;
+//    имя месторождения не меняется
+//
+//    Date startTime; - final Date startTime;
+//    начальная дата не меняется
+//
+//    Long averageSpeed; averageSpeed = null;
+//    добавил присвоение null после использования
+//
+//    Long duration; duration = null;
+//    добавил присвоение null после использования
+//
+//    Long averageDepth; averageDepth = null;
+//    добавил присвоение null после использования
+//
+//    int sum; sum = -1;
+//    присвоил недопустимое значение
+//
+//    int length; length = -1;
+//    присвоил недопустимое значение
+//
+//    int index; перенес объявление в цикл
+//
+//    int weight -  добавлена проверка на отрицательные значения
+//
+//    int number; number = null;
+//    добавил присвоение null после использования
+//
+//    Long startSpeed; final Long startSpeed;
+//    начальная скорость не меняется
+
+
+
+
+
+
+}

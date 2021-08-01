6bb03feb5c6f568e82461db130bceb13dd51d69b
Vladimir <ivlasovvladimir@gmail.com>
2021-08-01 17:25:55 +0300

    11

--- src/com/company/Eleven.java
+++ src/com/company/Eleven.java
@@ -1,4 +1,12 @@
 package com.company;
 
 public class Eleven {
+    //связывание выполняется во время написания кода, путь котроллера он не меняется
+    private final static String PATH = "api/news/";
+
+    //связывание выполняется во время написания кода, запрос не меняется
+    private final static String SQL_GET_ALL_WELL = "api/well/getall";
+
+    //связывание выполняется во время работы программы, нужны объект достается из базы после через сериализуется от отправки
+    String jsonEventWithWell = getEventWithWellJson(event);
 }

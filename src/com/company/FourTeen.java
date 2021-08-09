cfae11391b8de9159cf46a1dd59872e0347ff5bc
Vladimir <51863950+VolodyaVlasov@users.noreply.github.com>
2021-08-09 19:06:29 +0300

    Update FourTeen.java

--- src/com/company/FourTeen.java
+++ src/com/company/FourTeen.java
@@ -1,8 +1,36 @@
 package com.company;
 
 public class FourTeen {
-    //комментарий перенесен в название метода
-    getJsonIdNameJson
+   // комментарий перенесен в название метода
+    // getJsonIdNameJson
 
+    // был исправлен комментарий, добавленна пометка TODO для удобства поиска
+
+    // комментарий перенесен в название метода
+    // createNewWell_expectedNotification
+
+    // комментарий перенесен в название метода
+    // deleteWell_expectedNotification
+
+    // комментарий перенесен в название метода
+    // createNewWellWithoutEvent_ExpectedError
+
+    // комментарий перенесен в название метода
+    // recalculateStartTime_expectedStartTimeEqualsFive
+
+    // комментарий перенесен в название метода
+    // createNewEventWithoutPermission
+
+    // комментарий перенесен в название метода
+    // getEntityWithoutRefresh
+
+    // комментарий перенесен в название метода
+    // getByIdWithoutRefreshAndCheckPermission
+
+    //добавлен комментарий о том что проверять права не нужно, так как они были проверенные ранее
+
+    //добавленн комментарий о том что менять возврощаемое значение null на пустой список не нужно, так как фронт обрабатывет его верно
+
+    //добавлен комментраий о том что вызов метода с рефрешем очень важен так как он распроксирует объект 
     
 }
